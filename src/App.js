31478b6f0552e1edc49eac17ef78881470ad5683
Jelena Marjanović <jelena.marjanovic.13@gmail.com>
2019-08-06 02:59:48 +0200

    Outputting lists: with state & keys

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'sadsf', name: 'Max', age: 28 },
+      { id: 'qwerq', name: 'Manu', age: 29 },
+      { id: 'zxvcc', name: 'Stephanie', age: 26 }
     ],
     showPersons: false
+  };
+
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+
+    persons.splice(personIndex, 1);
+
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -32,20 +41,14 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-          >
-            My Hobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person, index) => (
+            <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+            />
+          ))}
         </div>
       );
     }
