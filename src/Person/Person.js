eaeb399324aaa79be8dc6926de2ce2bd1ff42914
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2019-08-13 14:11:38 +0200

    Use CSS modules for styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,16 +1,10 @@
 import React from "react";
 
-import "./Person.css";
+import classes from "./Person.module.css";
 
 const Person = props => {
-	const style = {
-		"@media (min-width: 500px)": {
-			width: "450px"
-		}
-	};
-
 	return (
-		<div className="Person" style={style}>
+		<div className={classes.Person}>
 			<p onClick={props.click}>
 				I'm {props.name} and I am {props.age} years old!
 			</p>
